d7a7d6c7c4e921bb5e531677090693098602a540
YT <yleet99@users.noreply.github.com>
2020-06-01 19:04:11 -0400

    Update get-quote.py
    random quotes

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,14 @@
+import random
+
 def primary():
 
   f = open("quotes.txt")
   quotes = f.readlines()
+  last = 13
+  rnd = random.randint(0, last)
   f.close()
 
-  print(quotes[13])
+  print(quotes[rnd])
 
 if __name__== "__main__": 
   primary()
